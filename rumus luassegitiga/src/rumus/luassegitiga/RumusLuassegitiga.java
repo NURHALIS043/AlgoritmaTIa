e2f9ef8d941a8d14745e4f0c4069d3de30fcca90
NURHALIS043 <57221578+NURHALIS043@users.noreply.github.com>
2019-11-02 15:44:02 +0700

    Add files via upload

---
+++ rumus luassegitiga/src/rumus/luassegitiga/RumusLuassegitiga.java
@@ -0,0 +1,13 @@
+package rumus.luassegitiga;
+public class RumusLuassegitiga {
+   public static void main(String[] args) {
+   //menentukan luas segitiga
+   //luassegitiga=1/2alas*tinggi
+   int  luassegitiga,alas,tinggi;
+   alas=43 ;
+   tinggi=16 ;
+   luassegitiga=alas/2*tinggi;
+   System.out.println(luassegitiga);
+    }
+    
+}
